4e1443c0eb4c21bd2e8976073c6f589a98d7bc88
Aaditya Anil <aadityaanilk@gmail.com>
2020-05-30 14:40:30 +0530

    Shows weather in Kelvin, Fahrenheight and Celcius

--- Weather.py
+++ Weather.py
@@ -2,12 +2,15 @@ import requests
 
 city = input("Enter your city: ")
 
-url= f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid='
+fahren= f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid= &units=imperial'
+celcius= f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid= &units=metric'
 
-res= requests.get(url)
+def request(url):
+    res= requests.get(url)
+    data= res.json()
+    temperature = data['main']['temp']
+    return temperature
 
-data= res.json()
-
-temperature = data['main']['temp']
-
-print(f'Temperature in Kelvin = {temperature}')
+print(f'Temperature in Kelvin = {request(celcius)+273}')
+print(f'Temperature in Fahrenheight = {request(fahren)}')
+print(f'Temperature in Celcius = {request(celcius)}')
